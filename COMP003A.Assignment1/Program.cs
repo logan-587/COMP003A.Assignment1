8284faf5eeb410fa52032e2736d1f61fbf36ed74
logansmith <logansmith@LEM231-04>
2023-01-18 19:47:43 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,116 @@
+﻿/*
+ * Author: Logan Smith
+ * Course: COMP-003A
+ * Purpose: Assignment - Week 1
+ */
+
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things About Me:");
+            Console.WriteLine("1. My first name is Logan");
+            Console.WriteLine("2. My last name is Smith");
+            Console.WriteLine("3. I'm a math and business major");
+            Console.WriteLine("4. Really CIS but I don't like to say that part");
+            Console.WriteLine("5. Because every university and their mother puts IT in the business department");
+            Console.WriteLine("6. And it's a pain to say computers because everyone wants details");
+            Console.WriteLine("7. I ended up moving to Data Analytics so I could feel decent about adding a math degree");
+            Console.WriteLine("8. DA + Statistics pair pretty well, right?");
+            Console.WriteLine("9. I know Python and a bit of Java");
+            Console.WriteLine("10. So far, I like Java better than this");
+            Console.WriteLine("11. Sorry");
+            Console.WriteLine("12. I've been meaning to learn R");
+            Console.WriteLine("13. Octave too");
+            Console.WriteLine("14. But motivation is hard");
+            Console.WriteLine("15. At least my degree will make me learn it");
+            Console.WriteLine("16. My math degree, that is");
+            Console.WriteLine("17. Python's my favorite");
+            Console.WriteLine("18. You don't have do a thing");
+            Console.WriteLine("19. As I'm sure you probably know");
+            Console.WriteLine("20. I started out at Cal Poly Pomona");
+            Console.WriteLine("21. But through a series of events that didn't end up working out");
+            Console.WriteLine("22. Long story short I could never get housing and rent is BAD down there");
+            Console.WriteLine("23. So I transferred to Fresno");
+            Console.WriteLine("24. And now my new math degree needs a CS course");
+            Console.WriteLine("25. To show mathematical ability outside of the typical dimensions of math");
+            Console.WriteLine("26. I like this a lot better than physics though");
+            Console.WriteLine("27. Mostly because I have programming experience");
+            Console.WriteLine("28. I was never good at the Object Oriented concepts though");
+            Console.WriteLine("29. So being drug, kicking and screaming as it were,");
+            Console.WriteLine("30. Is probably a good thing");
+            Console.WriteLine("31. My favorite programming hobby is Project Euler");
+            Console.WriteLine("32. When I'm not taking 24 units, that is");
+            Console.WriteLine("33. Yep. RIP me");
+            Console.WriteLine("34. I haven't gotten very far though");
+            Console.WriteLine("35. Because the math gets too complex");
+            Console.WriteLine("36. I can set up a nice lil brute force program for things");
+            Console.WriteLine("37. But everything 'should be solved in one minute or less' ");
+            Console.WriteLine("38. So after about 10 minutes and several thousand iterations I can tell things aren't working out");
+            Console.WriteLine("39. So that's a work in progress");
+            Console.WriteLine("40. They're all math problems by the way");
+            Console.WriteLine("41. I'd mention it earlier but you want line numbers");
+            Console.WriteLine("42. I've never been a game-code type person");
+            Console.WriteLine("43. I used to think I was, but honestly");
+            Console.WriteLine("44. 'Code a game, make your own game!' it's just not my speed");
+            Console.WriteLine("45. I think it's an instant gratification thing");
+            Console.WriteLine("46. I just don't get the rush from trying those out");
+            Console.WriteLine("47. But Project Euler telling me my answer is right?");
+            Console.WriteLine("48. *That's* the stuff");
+            Console.WriteLine("49. This is getting a little old, sorry");
+            Console.WriteLine("50. You get 50! That's pretty good! Here's some Dictionary.com Word of the Days:");
+            Console.WriteLine("51. Sklent");
+            Console.WriteLine("52. Nainsook");
+            Console.WriteLine("53. Dithyrambic");
+            Console.WriteLine("54. Welkin");
+            Console.WriteLine("55. Asinine");
+            Console.WriteLine("56. bombogenesis");
+            Console.WriteLine("57. schmatte");
+            Console.WriteLine("58. snye");
+            Console.WriteLine("59. malinger");
+            Console.WriteLine("60. jeu d'esprit");
+            Console.WriteLine("61. If you'd let me I'd love to just make a for loop of the number and random output :(");
+            Console.WriteLine("62. steppe");
+            Console.WriteLine("63. horchata");
+            Console.WriteLine("64. germinant");
+            Console.WriteLine("65. eristic");
+            Console.WriteLine("66. acmesthesia");
+            Console.WriteLine("67. Janus word");
+            Console.WriteLine("68. epigenetic");
+            Console.WriteLine("69. rejuvenate");
+            Console.WriteLine("70. vaudervillian");
+            Console.WriteLine("71. ignescent");
+            Console.WriteLine("72. smaragdine");
+            Console.WriteLine("73. Now THAT's a fun one");
+            Console.WriteLine("74. it means emerald-green");
+            Console.WriteLine("75. rubicate");
+            Console.WriteLine("76. precocity");
+            Console.WriteLine("77. jamboree");
+            Console.WriteLine("78. luminaria");
+            Console.WriteLine("79. lebkuchen");
+            Console.WriteLine("80. esculent");
+            Console.WriteLine("81. brume");
+            Console.WriteLine("82. posole");
+            Console.WriteLine("83. niveous");
+            Console.WriteLine("84. bobeche");
+            Console.WriteLine("85. shammes");
+            Console.WriteLine("86. hauteur");
+            Console.WriteLine("87. deglutition");
+            Console.WriteLine("88. androsphinx");
+            Console.WriteLine("89. dwy");
+            Console.WriteLine("90. momentous");
+            Console.WriteLine("91. utilidor");
+            Console.WriteLine("92. oleoresinous");
+            Console.WriteLine("93. cistvaen");
+            Console.WriteLine("94. algorithm");
+            Console.WriteLine("95. zaffer");
+            Console.WriteLine("96. barococo");
+            Console.WriteLine("97. morie");
+            Console.WriteLine("98. mehndi");
+            Console.WriteLine("99. decalcomania");
+            Console.WriteLine("100. netsuke");
+        }
+    }
+}
